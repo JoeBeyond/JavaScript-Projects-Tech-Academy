e4a5a060b174f0a48c8a706e35b58df6bbc50665
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-02-21 19:18:30 -0800

    Update main.js

--- Basic_JavaScript_Projects/JS/main.js
+++ Basic_JavaScript_Projects/JS/main.js
@@ -3,4 +3,15 @@ document.write("HELLO, Planet Alpha-");
 var A = 51-39;
 document.write(A);
 var B = 100;
-document.write(" Hi, I\'m Joe")
+document.write(" Hi, I\'m Joe!")
+document.write(" 5\$ for a meal"
+    + " that includes 3 tacos \& 3 lime slices.")
+var C = (" 50\% off the first 1000 tacos!"
+    + " Come get \'em while the tacos are fresh!")
+document.write(C);
+var D = "Combo Deals", Combo1 = " Rice and Beans Plate with 2 Corn Tortillas.", Combo2 = " 2 Tacos and 1 Bean, Rice and Cheese Burrito.",
+    Combo3 = " 1 Chile Relano with Rice and Beans.";
+document.write(" We've chosen a combo-meal for you...")
+document.write(Combo2);
+document.write(" That is 10.00\$ please. ")
+document.write(3+7)
