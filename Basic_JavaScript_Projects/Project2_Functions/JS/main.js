de88a9e121ff2e4f79e37d994ef5e248e032efb9
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-02-22 13:26:53 -0800

    Operator Assignment

--- Basic_JavaScript_Projects/Project2_Functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_Functions/JS/main.js
@@ -2,3 +2,8 @@ var A = 1000
 function myButton(){
     document.getElementById("Button").innerHTML = A;
 }
+function myButton2(){
+    var sentence = "My cat is"
+    sentence += " at the window."
+    document.getElementById("Concatenated String").innerHTML = sentence;
+}
