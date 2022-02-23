f71bc9a5d11abfdd6532fb432f9dc1c116768a04
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-02-22 18:35:40 -0800

    Modulus Operator Assignment

--- Basic_JavaScript_Projects/Project3_Math_Operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_Math_Operators/JS/main.js
@@ -1,4 +1,29 @@
-function hoverEffect(){
+function hoverEffect1(){
     var addition = 5 + 5;
-    document.getElementById("Math").innerHTML = "5 + 5 = " + addition;
+    document.getElementById("Math1").innerHTML = "5 + 5 = " + addition; //Function for Addition
+}
+
+function clickEffect1(){
+    var Subtraction = 10 - 5;
+    document.getElementById("Math2").innerHTML = "10 - 5 = " + Subtraction; //Function for Subtraction; notice it is Subtraction with a capitalized S
+}
+
+function hoverEffect2(){
+    var multiplication = 5 * 5;
+    document.getElementById("Math3").innerHTML = "5 * 5 = " + multiplication; //Function for Multiplication
+}
+
+function clickEffect2(){
+    var Division = 25 / 5;
+    document.getElementById("Math4").innerHTML = "25 / 5 = " + Division; //Function for Division
+}
+
+function hoverEffect3(){
+    var mathTime = (5+5) * 4 / 2 * 3; //Arithmetic Operations Function
+    document.getElementById("Math5").innerHTML = "5 plus 5, times 4, divided by 2, then multiplied by 3 equals " + mathTime
+}
+
+function clickEffect3(){
+    var moduleOperator = 50 % 7; //Module Operator
+    document.getElementById("Math6").innerHTML = "When you divide 50 by 7, you have a remainder of: " + moduleOperator;
 }
