25e06ff988f24864d5cc20d38810165098c1e498
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-02-23 17:18:17 -0800

    NaN Challenge

--- Basic_JavaScript_Projects/Project5_Comparisons_Type_Coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_Comparisons_Type_Coercion/JS/main.js
@@ -1,2 +1,14 @@
 document.write(typeof "Word");
 document.write("A"+1);
+
+function notNumber1(){
+    document.getElementById("NaN1").innerHTML = 0/0
+}
+
+function notNumber2(){
+    document.getElementById("NaN2").innerHTML = isNaN('Apple')
+}
+
+function notNumber3(){
+    document.getElementById("NaN3").innerHTML = isNaN('101')
+}
