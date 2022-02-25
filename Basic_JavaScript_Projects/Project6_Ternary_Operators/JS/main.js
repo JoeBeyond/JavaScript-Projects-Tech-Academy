4cbe3a2c907f4431d4b298afc1f4b4bcd481b8ab
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-02-25 11:45:04 -0800

    Ternary Operators Challenge

--- Basic_JavaScript_Projects/Project6_Ternary_Operators/JS/main.js
+++ Basic_JavaScript_Projects/Project6_Ternary_Operators/JS/main.js
@@ -4,3 +4,10 @@ function Ride_Function(){
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+function Vote_Function(){
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are not old enough":"You are old enough";
+    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+}
