2b4d9ad4f322396c2428be16c3c0792b8575624a
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-02-24 21:33:09 -0800

    Triple Equal Signs Assignment

--- Basic_JavaScript_Projects/Project5_Comparisons_Type_Coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_Comparisons_Type_Coercion/JS/main.js
@@ -1,3 +1,22 @@
+A = 5;
+document.write(A === A);
+document.write("<br>");
+
+B = 5;
+C = "10";
+document.write(B === C);
+document.write("<br>")
+
+D = 5;
+E = "5";
+document.write(D === E);
+document.write("<br>")
+
+F = 5;
+G = 10;
+document.write(F === G);
+document.write("<br>")
+
 document.write(typeof "Word" + "<br>");
 document.write("A"+1 + "<br>");
 document.write(2E310 + "<br>");
