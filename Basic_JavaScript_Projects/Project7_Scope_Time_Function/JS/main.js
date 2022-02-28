dd1d463e7ae57f0df5e4f30cdba5491a3257f2ea
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-02-27 20:38:24 -0800

    ELSE IF Assignment

--- Basic_JavaScript_Projects/Project7_Scope_Time_Function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_Scope_Time_Function/JS/main.js
@@ -40,11 +40,26 @@ function ifElse1() {
     Sides = document.getElementById("Sides").value
     if (Sides != 8) {
         Display = "Incorrect.";
-    }
+    } //IF, ELSE, Function that asks the user to enter input correctly and displays result strings
     else {
         Display = "You are Correct!"
     }
     document.getElementById("Guess").innerHTML = Display;
+}
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
 
 addNumbers1(); //Display results of Class
