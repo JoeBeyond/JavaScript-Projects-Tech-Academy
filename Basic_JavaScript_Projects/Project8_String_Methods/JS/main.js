e3b7e961b761d2dbed820a49452bbfa040d42ab6
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-03-02 14:16:08 -0800

    More Methods Challenge

--- Basic_JavaScript_Projects/Project8_String_Methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_String_Methods/JS/main.js
@@ -12,3 +12,15 @@ function sliceMethod() {
     var Section = Sentence.slice(10,20);
     document.getElementById("Slice").innerHTML = Section; //Function utilizing slice method to display a chunk of string
 }
+
+function upperCase() {
+    let Blessing = "plus 3 constitution"
+    let result = Blessing.toLocaleUpperCase(); //Function utilizing uppercase method
+    document.getElementById("Caps").innerHTML = result;
+}
+
+function search_Function() {
+    let text = "The river ran dry.";
+    let position = text.search("river"); //Function utilizing search method
+    document.getElementById("Search").innerHTML = position + " Gold Coins!";
+}
