cff3afb72839c958199bb3fee9d4d3181688d3fe
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-03-04 14:13:42 -0800

    For Loop Assignment

--- Basic_JavaScript_Projects/Project10_Loops_Array/JS/main.js
+++ Basic_JavaScript_Projects/Project10_Loops_Array/JS/main.js
@@ -13,3 +13,13 @@ function string_Length1() { //Function that returns the length of a string as a 
     let length = text.length;
     document.getElementById("stringlength").innerHTML = length;
 }
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+} //Function; For Loop that iterates through a list
