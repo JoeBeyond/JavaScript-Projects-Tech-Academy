fbd037af9d2acca5cd4d8c450077c24a41bdf750
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-03-05 23:02:17 -0800

    Switch Statement Assignment

---
+++ JavaScript_Projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,29 @@
+function Color_Function() { //Switch Statement that gives a list of colors with the user able to input a choice
+    var Color_Output;
+    var Colors = document.getElementById("Color_Input").value;
+    var Color_String = " is a great color!";
+    switch(Colors) {
+        case "Spring Peony":
+            Color_Output = "Spring Peony" + Color_String;
+        break;
+        case "Burnt Orange":
+            Color_Output = "Burnt Orange" +
+            Color_String;
+        break;
+        case "Wild Lavender":
+            Color_Output = "Wild Lavender" + Color_String;
+        break;
+        case "Sea Green":
+            Color_Output = "Sea Green" + Color_String;
+        break;
+        case "Golden Indigo":
+            Color_Output = "Golden Indigo" + Color_String;
+        break;
+        case "Rustic Magenta":
+            Color_Output = "Rustic Magenta" + Color_String;
+        break;
+        default:
+        Color_Output = "Please enter a color exacly as written on the above list.";
+    }
+    document.getElementById("Output").innerHTML = Color_Output;
+}
