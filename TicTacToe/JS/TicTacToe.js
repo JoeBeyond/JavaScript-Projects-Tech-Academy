564211b90155077b331533beb98332f5a111916b
KOSmicCaptain <97361279+KOSmicCaptain@users.noreply.github.com>
2022-03-09 21:53:10 -0800

    3T Part 7 Assignment

--- TicTacToe/JS/TicTacToe.js
+++ TicTacToe/JS/TicTacToe.js
@@ -141,3 +141,80 @@ function audio(audioURL)    {
     // Play method plays our audio sound.
     audio.play();
 }
+
+// function utilizes html canvas to draw win lines.
+function drawWinLine(coordX1, coordY1, coordX2, coordY2)    {
+    // This line accesses out HTML canvas element.
+    const canvas = document.getElementById('win-lines')
+    // This line indicates where the start of a lines x axis is.
+    let x1 = coordX1,
+        // This line indicates where the start of a lines y axis is.
+        y1 = coordY1, 
+        // This line indicates where the end of the lines x axis is.
+        x2 = coordX2,
+        // This line indicates where the end of the lines y axis is.
+        y2 = coordY2,
+        // This variable stores temporary x axis data we update in our animation loop.
+        x = x1,
+        // This variable stores temporary y axis data we update in our animation loop.
+        y = y1;
+
+
+    
+
+    // This function interacts with the canvas.
+    function animateLineDrawing()   {
+        // This variable creates a loop.
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        // This method clears content from last loop iteration.
+        c.clearRect(0, 0, 608, 608)
+        // This method starts a new path.
+        c.beginPath();
+        // This method moves us to a starting point for our line.
+        c.moveTo(x1, y1)
+        // This method indicates the end point of our line.
+        c.lineTo(x, y)
+        // This method sets the width of our line.
+        c.lineWidth = 10;
+        // This method sets the color of our line.
+        c.strokeStyle = 'rgba(70, 255, 33, .8)';
+        // This method draws everything we laid out above.
+        c.stroke();
+        // This condition checks if we've reached the endpoint.
+        if (x1 <= x2 && y1 <= y2) {
+            // This condition adds 10 to the previous end x point.
+            if (x < x2) { x += 10; }
+            // This condition adds 10 to the previous end y point.
+            if (y < y2) { y += 10; }
+            // This condition cancels our animation loop if we've reached the end points.
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+        }
+        // This condition is similar to the one above.
+        // This is necessary for the 6, 4, 2, win condition.
+        if (x1 <= x2 && y1 >= y2)   {
+            if (x < x2) { x += 10; }
+            if (y > y2) { y -= 10; }
+            if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+        }
+    }
+
+
+
+
+
+    // This  function clears out canvas after our win line is drawn.
+    function clear() {
+        // This line starts our animation loop.
+        const animationLoop = requestAnimationFrame(clear);
+        // This line clears our canvas.
+        c.clearRect(0, 0, 608, 608);
+        // This line stops our animation loop.
+        cancelAnimationFrame(animationLoop);
+    }
+    // This line disallows clicking while the win sound is playing.
+    disableClick();
+    // This line calls our main animation loop.
+    animateLineDrawing();
+    // This line waits 1 second. Then, clears canvas, resets game, and allows clicking again.
+    setTimeout(function () { clear(); resetGame(); }, 1000);
+}
